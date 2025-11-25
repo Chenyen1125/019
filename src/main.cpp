cb7844b31f5ad80c71451e129935a2be57a2183d
Chenyen125 <11312019@st.hhvs.tn.edu.tw>
2025-11-25 14:29:33 +0800

    新增 LED 控制功能，實現每兩秒閃爍一次

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <Arduino.h>
 
+// LED on GPIO22, blink once every 2 seconds (1s on, 1s off)
+const int LED_PIN = 22;
+
 void setup() {
-  
+	pinMode(LED_PIN, OUTPUT);
+	digitalWrite(LED_PIN, LOW);
 }
 
 void loop() {
-  
+	digitalWrite(LED_PIN, HIGH); // LED on
+	delay(1000);                 // wait 1 second
+	digitalWrite(LED_PIN, LOW);  // LED off
+	delay(1000);                 // wait 1 second
 }
